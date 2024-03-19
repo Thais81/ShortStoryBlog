6b5b7e03bb40715892bc60d8a39ffea3e9381f6b
Amelie Pruvost <ameliepruvost@hotmail.fr>
2024-03-19 09:24:13 +0100

    création du fichier DAOFactory avec Vote, Commentaire, Utilisateur et Nouvelle

---
+++ src/main/java/DAO/DAOFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+/**
+ *
+ * @author Amelie Solanas Pruvost
+ */
+public final class DAOFactory {
+
+    private static NouvelleDAO nouvelleDAO;
+    private static VoteDAO voteDAO;
+    private static CommentaireDAO commentaireDAO;
+    private static UtilisateurDAO utilisateurDAO;
+
+    public static DAOFactory() {
+    }
+
+    public static NouvelleDAO getNouvelleDAO() {
+        if (nouvelleDAO == null) {
+            nouvelleDAO = new NouvelleDAO();
+        }
+        return nouvelleDAO;
+    }
+
+    public static VoteDAO getVoteDAO() {
+        if (voteDAO == null) {
+            voteDAO = new VoteDAO();
+        }
+        return voteDAO;
+    }
+
+    public static CommentaireDAO geCommentaireDAO() {
+        if (commentaireDAO == null) {
+            commentaireDAO = new CommentaireDAO();
+        }
+        return commentaireDAO;
+    }
+
+    public static UtilisateurDAO getUtilisateurDAO() {
+        if (utilisateurDAO == null) {
+            utilisateurDAO = new UtilisateurDAO();
+        }
+        return utilisateurDAO;
+    }
+
+}
